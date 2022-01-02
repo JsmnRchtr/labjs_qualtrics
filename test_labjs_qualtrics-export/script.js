89fe97f098a8409fdfe573f0ccfa0227c643c080
JsmnRchtr <94963292+JsmnRchtr@users.noreply.github.com>
2022-01-02 11:17:40 +0100

    Add files via upload
    Added labjs experiment files

---
+++ test_labjs_qualtrics-export/script.js
@@ -0,0 +1,79 @@
+// Define study
+const study = lab.util.fromObject({
+  "title": "root",
+  "type": "lab.flow.Sequence",
+  "parameters": {},
+  "plugins": [
+    {
+      "type": "lab.plugins.Metadata",
+      "path": undefined
+    },
+    {
+      "type": "lab.plugins.PostMessage",
+      "path": undefined
+    }
+  ],
+  "metadata": {
+    "title": "test_labjs_qualtrics",
+    "description": "",
+    "repository": "",
+    "contributors": "Jasmin"
+  },
+  "files": {},
+  "responses": {},
+  "content": [
+    {
+      "type": "lab.html.Page",
+      "items": [
+        {
+          "type": "text",
+          "content": "Hello!"
+        }
+      ],
+      "scrollTop": true,
+      "submitButtonText": "Continue →",
+      "submitButtonPosition": "right",
+      "files": {},
+      "responses": {
+        "": ""
+      },
+      "parameters": {},
+      "messageHandlers": {},
+      "title": "Page"
+    },
+    {
+      "type": "lab.html.Form",
+      "content": "\u003Cform\u003E\n  \u003Cinput name=\"variable\"\u003E\n  \u003Cbutton type=\"submit\"\u003ESubmit\u003C\u002Fbutton\u003E\n\u003C\u002Fform\u003E",
+      "scrollTop": true,
+      "files": {},
+      "responses": {
+        "": ""
+      },
+      "parameters": {},
+      "messageHandlers": {},
+      "title": "Form"
+    },
+    {
+      "type": "lab.html.Page",
+      "items": [
+        {
+          "type": "text",
+          "content": "Thanks!"
+        }
+      ],
+      "scrollTop": true,
+      "submitButtonText": "Continue →",
+      "submitButtonPosition": "right",
+      "files": {},
+      "responses": {
+        "": ""
+      },
+      "parameters": {},
+      "messageHandlers": {},
+      "title": "Page"
+    }
+  ]
+})
+
+// Let's go!
+study.run()
